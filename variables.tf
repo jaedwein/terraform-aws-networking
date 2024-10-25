ce270e126b822d91cc24eed3176e40d8aab8ba11
Javier Weinmeister <jaedwein@gmail.com>
2024-10-25 00:44:55 -0300

    Cambio de red Cambio de red

--- variables.tf
+++ variables.tf
@@ -1,13 +1,13 @@
 variable "aws_region" {}
 
 variable "vpc_cidr" {
-  default = "10.123.0.0/16"
+  default = "10.24.0.0/16"
 }
 variable "public_cidrs" {
-  default = ["10.123.1.0/24", "10.123.3.0/24"]
+  default = ["10.24.1.0/24", "10.24.3.0/24"]
 }
 variable "private_cidrs" {
-  default = ["10.123.2.0/24", "10.123.4.0/24", "10.123.6.0/24"]
+  default = ["10.24.2.0/24", "10.24.4.0/24", "10.24.6.0/24"]
 }
 variable "public_sn_count" {
   default = 2
